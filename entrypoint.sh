79624ea1f38ce2d4a368c920cbb23444ae3e7788
Michael Fung <hkuser2001@gmail.com>
2022-06-08 13:38:35 +0800

    update for rt image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 CMD=$1
 
-# setup perl environment
-#export PATH=/opt/perlbrew/bin:/opt/perlbrew/perls/perl-5.28.3/bin:$PATH
-
 # source /app/devel.rc if exists
 [ -f "/app/runtime.rc" ] && source /app/runtime.rc
 
